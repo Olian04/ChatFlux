a296dfbc79fd9256ffb32052ab4f9818a3b4440e
Oliver <rift95@live.se>
2021-09-20 10:46:35 +0200

    added variable execution time to tests

--- tests/util/inMemoryDatabase.ts
+++ tests/util/inMemoryDatabase.ts
@@ -1,6 +1,10 @@
 import { Identifier, SimpleDatabase } from '../../src/api';
 import { CountState } from './countTypes';
 
+//  Simulate async operations
+const sleep = () =>
+  new Promise((resolve) => setTimeout(resolve, 5 + 5 * Math.random()));
+
 export const createDB = (
   storage: Map<Identifier, CountState>
 ): SimpleDatabase<CountState> => {
@@ -10,13 +14,19 @@ export const createDB = (
       if (!maybeData) {
         throw new Error(`Unknown ID: ${id}`);
       }
+      await sleep();
       return maybeData;
     },
     set: async (id, newData) => {
+      await sleep();
       storage.set(id, newData);
     },
-    has: async (id) => storage.has(id),
+    has: async (id) => {
+      await sleep();
+      return storage.has(id);
+    },
     delete: async (id) => {
+      await sleep();
       storage.delete(id);
     },
   };
